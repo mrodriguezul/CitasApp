719de2fbf1d970a6d36241f9d9faad7beb5da7da
mrodriguezul <miguel.rodriguez.u@gmail.com>
2025-08-01 03:51:14 -0500

    Actualiza respuestas con ResponseEntity

--- src/main/java/com/mrodriguezul/citasapp/web/controller/IdentificacionController.java
+++ src/main/java/com/mrodriguezul/citasapp/web/controller/IdentificacionController.java
@@ -3,36 +3,45 @@ package com.mrodriguezul.citasapp.web.controller;
 import com.mrodriguezul.citasapp.domain.Identificacion;
 import com.mrodriguezul.citasapp.domain.service.IdentificacionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
-@RequestMapping("/identificaciones")
+@RequestMapping("/identificacion")
 public class IdentificacionController {
 
     @Autowired
     private IdentificacionService identificacionService;
 
     @GetMapping("")
-    List<Identificacion> listarIdentificaciones(){
-        return identificacionService.findAll();
+    ResponseEntity<List<Identificacion>> listarIdentificaciones(){
+        return new ResponseEntity<>(identificacionService.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{idIdentificacion}")
-    public Optional<Identificacion> obtenerIdentificacion(@PathVariable("idIdentificacion") Long idIdentificacion){
-        return identificacionService.findById(idIdentificacion);
+    public ResponseEntity<Identificacion> obtenerIdentificacion(@PathVariable("idIdentificacion") Long idIdentificacion){
+        return identificacionService.findById(idIdentificacion).map(identificacion -> {
+            return new ResponseEntity<>(identificacion, HttpStatus.OK);
+        }).orElseGet(() -> {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        });
     }
 
     @PostMapping()
-    public Identificacion guardarIdentificacion(@RequestBody Identificacion identificacion){
-        return identificacionService.save(identificacion);
+    public ResponseEntity<Identificacion> guardarIdentificacion(@RequestBody Identificacion identificacion){
+        return new ResponseEntity<>(identificacionService.save(identificacion), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{idIdentificacion}")
-    public boolean eliminarIdentificacion(@PathVariable("idIdentificacion") Long idIdentificacion){
-        return identificacionService.delete(idIdentificacion);
+    public ResponseEntity eliminarIdentificacion(@PathVariable("idIdentificacion") Long idIdentificacion){
+        if(identificacionService.delete(idIdentificacion)){
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
     }
 
 }
