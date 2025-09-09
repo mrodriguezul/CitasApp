fb237d8bf63786f3ecd2d4e010999ea97ff307b5
mrodriguezul <miguel.rodriguez.u@gmail.com>
2025-09-09 03:47:24 -0500

    Implementación de roles y permisos en memoria

--- src/main/java/com/mrodriguezul/citasapp/config/SecurityConfig.java
+++ src/main/java/com/mrodriguezul/citasapp/config/SecurityConfig.java
@@ -2,9 +2,16 @@ package com.mrodriguezul.citasapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,6 +19,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/auth/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                .requestMatchers(HttpMethod.GET, "/identificacion/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/doctor/**").hasAnyRole("ADMIN", "CUSTOMER")
+                .requestMatchers(HttpMethod.GET, "/paciente/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/**").denyAll()
                 .anyRequest()
                 //.permitAll()
                 .authenticated()
@@ -20,5 +34,26 @@ public class SecurityConfig {
         .cors(AbstractHttpConfigurer::disable)
         .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User
+                .withUsername("admin")
+                .password(passwordEncoder().encode("qwerty123"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails customer = User
+                .withUsername("customer")
+                .password(passwordEncoder().encode("qwerty123"))
+                .roles("CUSTOMER")
+                .build();
+        return new InMemoryUserDetailsManager(admin, customer);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
